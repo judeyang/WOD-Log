1d0b7fffc7e2d72be1acb4dd7ebd55bee4c4312a
JudeYang <byoox@qq.com>
2017-04-19 23:49:59 +0800

    implement step 5

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,13 @@ Rails.application.routes.draw do
   root 'jobs#index'
 
   namespace :admin do
-    resources :jobs
-  end
+      resources :jobs do
+        member do
+          post :publish
+          post :hide
+        end
+      end
+    end
+
 
 end
